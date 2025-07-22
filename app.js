f0580625b849d9bf676b6f95f1c09cabbc5046fd
andersonfuzz <andrade.28me@gmail.com>
2025-07-21 22:13:36 -0300

    cors

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import cors from "cors";
 import TaskRouter from './routes/TaskRouter.js';
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/tasks', TaskRouter)
 
